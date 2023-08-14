ef8f9c2072c67608cb8aebfe6b61ff3c0b84bdad
unknown <you@example.com>
2023-08-14 15:55:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');// the server is allowing requests from any origin
+  res.setHeader('Access-Control-Allow-Methods','*');//he server is allowing requests using any HTTP method (GET, POST, PUT, DELETE, etc.)
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');//it allows the "Content-Type" and "Authorization" headers to be included in the request.
+  next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/node-app').then(result => {
+    app.listen(8080);
+  }).catch(err => 
+      console.log(err)
+    );
